e9c9920532db9a2439b66943338c8e09b628511f
David Millar <dave@davegoesthedistance.com>
2022-01-20 18:31:51 -0600

    Added Dallas Comedy Club

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // jshint esversion:6
 var me = self,
-    CACHE_NAME = 'dfwv3.6900007',
+    CACHE_NAME = 'dfwv3.6900008',
     urlsToCache = [
         '/',
         '/?utm_source=web_app_manifest',
